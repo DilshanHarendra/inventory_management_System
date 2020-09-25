c95975c6a26b475be31731cd2facc2d78ce4ef77
sathiralamal <sdsat756@gmail.com>
2020-09-25 11:24:16 +0530

    add report generator navigation button

--- app/src/main/java/com/codefour/uee/MainActivity.java
+++ app/src/main/java/com/codefour/uee/MainActivity.java
@@ -2,13 +2,33 @@ package com.codefour.uee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    String TAG="MainActivity";
+
+    Button reportGenBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        reportGenBtn=findViewById(R.id.repotgen_button);
+        this.navigateToReportGenerator();
+    }
+
+    public void navigateToReportGenerator(){
+        reportGenBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent reportGenerator =new Intent(getApplicationContext(),ReportGenerate.class);
+                startActivity(reportGenerator);
+                Log.i(TAG,"navigate to report generator");
+            }
+        });
     }
 }
