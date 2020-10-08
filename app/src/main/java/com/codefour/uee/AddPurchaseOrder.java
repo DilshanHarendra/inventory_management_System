7bd0d202cf939b5d02defc8eb8f5a9ad323afd16
VidulaDakshitha <viduladakshitha@gmail.com>
2020-10-08 23:22:48 +0530

    changed login

--- app/src/main/java/com/codefour/uee/AddPurchaseOrder.java
+++ app/src/main/java/com/codefour/uee/AddPurchaseOrder.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
+
+import com.codefour.uee.Popup.ShowPopups;
 
 public class AddPurchaseOrder extends AppCompatActivity {
 
     ImageButton purchaseBack;
+
+    Button purchaseSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +25,28 @@ public class AddPurchaseOrder extends AppCompatActivity {
 
         purchaseBack=findViewById(R.id.purchaseBack);
 
+        purchaseSave=findViewById(R.id.purchaseSave);
+
+
         purchaseBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(AddPurchaseOrder.this,ViewPurchaseOrder.class));
+            }
+        });
+
+
+        purchaseSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                ShowPopups showPopups=new ShowPopups();
+                LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                showPopups.ShowSuccesfulPopup(v,inflater).showAtLocation(v, Gravity.CENTER, 0, 0);
+
+
             }
         });
     }
--- app/src/main/java/com/codefour/uee/Regsiter.java
+++ app/src/main/java/com/codefour/uee/Regsiter.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageButton;
+
+import com.codefour.uee.Popup.ShowPopups;
 
 public class Regsiter extends AppCompatActivity {
 
     private ImageButton registerback;
+
+    private Button registerbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +27,29 @@ public class Regsiter extends AppCompatActivity {
 
         registerback=findViewById(R.id.backRegister);
 
+        registerbtn=findViewById(R.id.registerbtn);
+
         registerback.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(Regsiter.this,Login.class));
+            }
+        });
+
+
+
+
+        registerbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+
+                ShowPopups showPopups=new ShowPopups();
+                LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+                showPopups.ShowSuccesfulPopup(v,inflater).showAtLocation(v, Gravity.CENTER, 0, 0);
+
+
             }
         });
     }
